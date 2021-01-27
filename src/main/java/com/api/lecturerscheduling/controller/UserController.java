7e914a2acc991b8f1cdf3d7c7c61ad8b54d80f3b
haunguyenfptk13 <71393780+haunguyenfptk13@users.noreply.github.com>
2021-01-27 19:32:20 +0700

    1

--- src/main/java/com/api/lecturerscheduling/controller/UserController.java
+++ src/main/java/com/api/lecturerscheduling/controller/UserController.java
@@ -5,10 +5,7 @@ import com.api.lecturerscheduling.dto.UserDTO;
 import com.api.lecturerscheduling.dto.UserLoginDTO;
 import com.api.lecturerscheduling.service.CourseUserService;
 import com.api.lecturerscheduling.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -19,31 +16,36 @@ import java.util.Optional;
 @RequestMapping("/api/v1")
 public class UserController {
 
-    private final UserService userService;
-    private final CourseUserService courseUserService;
+//    private final UserService userService;
+//    private final CourseUserService courseUserService;
 
-    public UserController(UserService userService, CourseUserService courseUserService) {
-        this.userService = userService;
-        this.courseUserService = courseUserService;
-    }
+//    public UserController(UserService userService, CourseUserService courseUserService) {
+//        this.userService = userService;
+//        this.courseUserService = courseUserService;
+//    }
 
-    @PostMapping("/login")
-    public UserLoginDTO login(@RequestBody UserDTO dto) {
-        UserDTO userDTO = userService.login(dto);
-        List<CourseUserDTO> courseUserDTOList = courseUserService.getAllCourseUser(dto.getUsername());
-//        Optional.ofNullable(courseUserDTOList)
-//                .orElse(Collections.emptyList())
-//                .stream()
-//                .filter(Objects::nonNull)
-//                .map(item -> {
-//
-//                })
+//    @PostMapping("/login")
+//    public UserLoginDTO login(@RequestBody UserDTO dto) {
+//        UserDTO userDTO = userService.login(dto);
+//        List<CourseUserDTO> courseUserDTOList = courseUserService.getAllCourseUser(dto.getUsername());
+////        Optional.ofNullable(courseUserDTOList)
+////                .orElse(Collections.emptyList())
+////                .stream()
+////                .filter(Objects::nonNull)
+////                .map(item -> {
+////
+////                })
 
 
-        UserLoginDTO userLoginDTO = new UserLoginDTO();
-        userLoginDTO.setUserDTO(userDTO);
-        userLoginDTO.setCourseUserDTOList(courseUserDTOList);
-        return userLoginDTO;
+//        UserLoginDTO userLoginDTO = new UserLoginDTO();
+//        userLoginDTO.setUserDTO(userDTO);
+//        userLoginDTO.setCourseUserDTOList(courseUserDTOList);
+//        return userLoginDTO;
+//    }
+
+    @GetMapping("/")
+    public String login(){
+        return "login success";
     }
 
 }
